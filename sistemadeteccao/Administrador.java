a5166ef3c9e6f18a166788d545e7065304de9612
Larice <annelarice93@gmail.com>
2025-01-27 17:15:02 -0300

    Update Administrador.java

--- sistemadeteccao/Administrador.java
+++ sistemadeteccao/Administrador.java
@@ -4,42 +4,42 @@ package sistemadeteccao;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Administrador<MateriaisEscolares> {
-
-    private String nome; 
-    private List<MateriaisEscolares> MateriaisEscolares; 
-    private Deteccao deteccao; 
+public class Administrador {
+    private String nome;
+    private List<ObjetoEscolar> objetosEscolares;
+    private Deteccao deteccao;
 
     public Administrador(String nome) {
-        this.nome = nome; 
-        this.MateriaisEscolares = new ArrayList<>(); 
+        this.nome = nome;
+        this.objetosEscolares = new ArrayList<>();
         this.deteccao = new Deteccao();
     }
 
-    public void adicionarMateriaisEscolares(MateriaisEscolares objeto) {
-        MateriaisEscolares.add(objeto); 
-        System.out.println("Objeto escolar adicionado: " + ((Object) objeto).getNome());
+    public void adicionarObjetoEscolar(ObjetoEscolar objeto) {
+        objetosEscolares.add(objeto);
+        System.out.println("Objeto escolar adicionado: " + objeto.getNome());
     }
 
     public void iniciarDeteccao() {
-        Deteccao.detectar(); 
+        deteccao.detectar();
     }
 
     public void exibirObjetosEscolares() {
         System.out.println("Objetos escolares cadastrados:");
-        for (MateriaisEscolares objeto : MateriaisEscolares) {
-            objeto.descreverObjeto(); 
+        for (ObjetoEscolar objeto : objetosEscolares) {
+            objeto.descreverObjeto();
         }
+    }
+
+    public Deteccao getDeteccao() {
+        return deteccao;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public static void main(String[] args) {
-            BancoDeDados banco = new BancoDeDados();
-            banco.testarConexao();
-        
+
     
    /* public static void main(String[] args) {
         BancoDeDados bancoDeDados = new BancoDeDados();
