45c0917cadc91a1617ecd2b68990c955eaa080af
Larice <annelarice93@gmail.com>
2025-01-27 20:19:57 -0300

    Update BancoDeDados.java

--- sistemadeteccao/BancoDeDados.java
+++ sistemadeteccao/BancoDeDados.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 
 
 public class BancoDeDados {
-    private static final String URL = "jdbc:sqlserver://127.0.0.1\\SQLEXPRESS;databaseName=banco_sistemadetecta;encrypt=true;trustServerCertificate=true";
-    private static final String USER = "LENOVOI5//annel"; // Substitua pelo seu usuário
+    private static final String URL = "";
+    private static final String USER = ""; // Substitua pelo seu usuário
     private static final String PASSWORD = "12345"; // Substitua pela sua senha
 
     public Connection conectar() throws SQLException {
@@ -22,12 +22,35 @@ public class BancoDeDados {
             stmt.setString(1, objeto.getNome());
             stmt.setString(2, objeto.getCategoria());
             stmt.executeUpdate();
-            System.out.println("Objeto salvo no banco de dados com sucesso: " + objeto.getNome());
+            System.out.println("Objeto salvo no banco de dados: " + objeto.getNome());
         } catch (SQLException e) {
             System.err.println("Erro ao salvar objeto no banco de dados: " + e.getMessage());
         }
     }
-    
+
+    public List<ObjetoEscolar> listarObjetos() {
+        List<ObjetoEscolar> objetos = new ArrayList<>();
+        String sql = "SELECT nome, categoria FROM materiais_escolares";
+        try (Connection conexao = conectar();
+             PreparedStatement stmt = conexao.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                String nome = rs.getString("nome");
+                String categoria = rs.getString("categoria");
+                objetos.add(new ObjetoEscolar(nome, categoria) {
+                    @Override
+                    public void descreverObjeto() {
+                        System.out.println("Objeto: " + nome + ", Categoria: " + categoria);
+                    }
+                });
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar objetos: " + e.getMessage());
+        }
+        return objetos;
+    }
+
     public void testarConexao() {
         try (Connection conexao = conectar()) {
             System.out.println("Conexão bem-sucedida!");
@@ -35,5 +58,4 @@ public class BancoDeDados {
             System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
         }
     }
-
 }
